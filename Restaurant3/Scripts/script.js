3d5c32f45d76afa988f8e143ab389525eb56512e
Hector Lliguichuzca <hectorlliguichuzca@hectors-mbp.lab.lagcc>
2019-04-23 15:04:18 -0400

    edited js in rest3

--- Restaurant3/Scripts/script.js
+++ Restaurant3/Scripts/script.js
@@ -10,6 +10,31 @@ var fname = document.forms["customerForm"]["fname"].value;
 var lname = document.forms["customerForm"]["lname"].value;
 var email = document.forms["customerForm"]["email"].value;
 var phone = document.forms["customerForm"]["phone"].value;
+var inquiry = document.forms["customerForm"]["inquiry"].value;
+
+
+
+var yes = document.forms["customerForm"]["Radio1"].checked;
+var no = document.forms["customerForm"]["Radio2"].checked;
+var hold;
+var m = document.forms["customerForm"]["m"].checked;
+var t = document.forms["customerForm"]["t"].checked;
+var w = document.forms["customerForm"]["w"].checked;
+var th = document.forms["customerForm"]["th"].checked;
+var f = document.forms["customerForm"]["f"].checked;
+
+
+if(yes == false && no == false ){
+alert("Have you been to The Ratatouille before");
+return false;
+}
+
+if(m == false && t == false && w == false && th == false && f ==false){
+  alert("Please select best days to contact you");
+  return false;
+}
+
+
 
 
 
@@ -44,16 +69,45 @@ if(phone == "" || isNaN(phone)){
 }
 
 
+if(inquiry == ""){
+  alert("Please Select Reason for Inquiry");
+  document.forms["customerForm"]["inquiry"].focus();
+  return false;
+}
 
 
 
 
-alert("You Entered The following Info: " + "\nName: " + fname + " " + lname + "\n" + "Email: " + email + "\nPhone: " + phone);
+alert("You Entered The following Info: " + "\nName: " + fname + " " + lname + "\n" + "Email: " + email + "\nPhone: " + phone + "\nInquiry Reason: " + inquiry);
+
+
+
+// var oRadio = document.forms[0].elements["gridCheck1"];
+//
+// for(var i = 0; i< oRadio.length;i++){
+//
+//   if(oRadio[i].checked)
+//   {
+//
+// console.log(oRadio[i].value);
+// }
+// }
 
 
 
 
-// document.getElementById("demo").innerHTML = "You Entered: "+ fname + " " + " " + lname + " " + " "  + " " + email + " "+ phone;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
